80337c4a3bec6659c9ad0ac362e7b281799e01be
soloFac <francopdlr@gmail.com>
2020-04-26 19:34:14 -0300

    mejoria en el tp

--- distribuidora.cpp
+++ distribuidora.cpp
@@ -24,7 +24,9 @@ Cliente * Interfaz();
 
 void CargarClientes(Cliente ** p_usuario, int NdeClientes);
 
-/*int CostoTotal();*/
+void CargarProductos(Cliente ** p_usuario1, int CantidadProductos);
+
+/*int CantidadPorPrecioUnitario();*/
 
 int main(){
     Cliente * asiduo;
@@ -32,7 +34,7 @@ int main(){
     asiduo = Interfaz();  //Consulta: Es mas recomendable hacer la reserva de memoria en el programa principal y tener siempre el puntero de la memoria reservada ahí?
                                 //Esta bien que haga esto??? Devolver de esta manera el puntero?
 
-    
+    printf("%s", *asiduo->Productos->TipoProducto);
 
     return 0;
 }
@@ -52,42 +54,55 @@ Cliente * Interfaz(){
 
 void CargarClientes(Cliente ** p_usuario, int NdeClientes){
     int ClienteID1, CantidadProductosAPedir;
+    Cliente * p_aux = * p_usuario;  //declaro un puntero auxiliar para enviarlo a la funcion CargarProductos
 
     for (int i = 0; i < NdeClientes; i++)
-    {  //Cargo los datos del Cliente
+    {  //------Cargo los datos del Cliente-------
         printf("Ingrese el ID de cliente Nro. %d: \n", i+1);
         scanf("%d", &(* p_usuario)[i].ClienteID);
         printf("El id del usuario es: %d\n", (* p_usuario)[i].ClienteID);
-        //printf("Ingrese el nombre: \n");
-        //(* p_usuario)[i].NombreCliente = (char *) malloc(sizeof(char) * 100);
-        //scanf("%s", &(((* p_usuario)[i].NombreCliente[0]);
-        /*(* p_usuario)[i].NombreCliente = getchar();*/
+        printf("Ingrese el nombre del cliente #%d: \n");
+        char *nom_aux = (char *) malloc(sizeof(char) * 100);    //v_aux para guardar el nombre saber la cantidad de caracteres y luego guardar el nombre del cliente
+        scanf("%s", nom_aux);
+        int tam_nombre = strlen(nom_aux);
+        (* p_usuario)[i].NombreCliente = (char *) malloc(sizeof(char) * tam_nombre);
+        strcpy((* p_usuario)[i].NombreCliente, nom_aux);
+        free(nom_aux);
+
         printf("Ingrese la cantidad de Productos a Pedir: ");
         scanf("%d", &(* p_usuario)[i].CantidadProductosAPedir);
-    //Cargo los datos del Producto del cliente generados aleatoriamente
-        //Consulta: Deberia separar para que el producto sea agregado en otra funcion?
-        (* p_usuario)[i].Productos = (Producto *) malloc(sizeof(Producto) * ((* p_usuario)[i].CantidadProductosAPedir));     //Reservo memoria para el Producto del Cliente
-        int aleatorio;
+
+        //-------Cargo los datos del Producto del cliente generados aleatoriamente----
+
+        (* p_usuario)[i].Productos = (Producto *) malloc(sizeof(Producto) * ((* p_usuario)[i].CantidadProductosAPedir));     //Reservo memoria para los Productos del Cliente
+        int CantidadProductos = (* p_usuario)[i].CantidadProductosAPedir;
+        Cliente * p_usuario1 = *p_usuario;
+
+        CargarProductos(&p_usuario1, CantidadProductos);
         
-        for (int j = 0; j < (* p_usuario)[i].CantidadProductosAPedir; j++)
-        {
-            aleatorio = rand() % 5;
-            (* p_usuario)[i].Productos->ProductoID = i+1;
-            (* p_usuario)[i].Productos->Cantidad = 1 + rand() % 11;    //Amo TL
-            int tamanio = strlen(TiposProductos[aleatorio]);
-            (* p_usuario)[i].Productos->TipoProducto = (char *)malloc(sizeof(tamanio)); //Consulta: Porque size of de tamanio si tamanio tiene la cantidad de caracteres, no seria lo mismo que poner tamanio directamente? porque cada char ocupa un byte?
-            strcpy((* p_usuario)[i].Productos->TipoProducto, TiposProductos[aleatorio]);
-            (* p_usuario)[i].Productos->PrecioUnitario = 10 + rand() % 91;
-            (* p_usuario)[i].Productos[j];
-        }
-            printf("Cantidad Productos A Pedir son %d \n", (* p_usuario)[i].CantidadProductosAPedir);
-            printf("Tipo de producto: %s \n", (* p_usuario)[i].Productos->TipoProducto);
     }
     
 
     getchar();
 };
+
+void CargarProductos(Cliente ** p_usuario1, int CantidadProductos){
+
+    int aleatorio;
+
+    for (int j = 0; j < CantidadProductos; j++)
+    {
+        aleatorio = rand() % 5;
+        (* p_usuario1)[0].Productos[j].ProductoID = j + 1;
+        (* p_usuario1)[0].Productos[j].Cantidad = 1 + rand() % 11;    // TL♥
+        int tamanio = strlen(TiposProductos[aleatorio]);
+        (* p_usuario1)[0].Productos[j].TipoProducto = (char *)malloc(sizeof(tamanio) * sizeof(char *)); //Consulta: Porque size of de tamanio si tamanio tiene la cantidad de caracteres, no seria lo mismo que poner tamanio directamente? porque cada char ocupa un byte?
+        strcpy((* p_usuario1)[0].Productos[j].TipoProducto, TiposProductos[aleatorio]);
+        (* p_usuario1)[0].Productos[j].PrecioUnitario = 10 + rand() % 91;
+    }      
+};
+
 /*
-int CostoTotal(){
+int CantidadPorPrecioUnitario(){
 
 };*/
