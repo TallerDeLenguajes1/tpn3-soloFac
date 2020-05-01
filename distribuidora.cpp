e46b628a3afceb386a3855949cba4796b0577c40
soloFac <francopdlr@gmail.com>
2020-05-01 07:04:43 -0300

    avance del tp

--- distribuidora.cpp
+++ distribuidora.cpp
@@ -24,22 +24,37 @@ Cliente * Interfaz();
 
 void CargarClientes(Cliente ** p_usuario, int NdeClientes);
 
-void CargarProductos(Cliente ** p_usuario1, int CantidadProductos, int i);
+void CargarProductos(Cliente ** p_usuario_aux, int CantidadProductos);
 
 /*int CantidadPorPrecioUnitario();*/
 
 int main(){
+    
+    time_t t;
+    srand((unsigned) time(&t));
+
     Cliente * asiduo;   //declaro una variable para recibir la direccion del puntero cliente
     
-    asiduo = Interfaz();  //Consulta: Es mas recomendable hacer la reserva de memoria en el programa principal y tener siempre el puntero de la memoria reservada ahí?
-                                //Esta bien que haga esto??? Devolver de esta manera el puntero?
+    asiduo = Interfaz();
 
-    printf("Los productos del primer cliente son \n");
-    printf("Precio unitario: %d", (* asiduo).Productos[0].PrecioUnitario);
+    printf("Probando: ");
+    //printf("El nombre del cliente es", (asiduo)[0].NombreCliente); //aqui me da algo de error de segmentacion
+    //scanf("%c");
 
-    asiduo--;
-    printf("El nombre del cliente es: ");
-    printf("%s", asiduo->NombreCliente);
+    for (int i = 0; i < 3; i++)
+    {
+        Producto * P = asiduo[i].Productos;
+        printf("cliente %s \n", asiduo[i].NombreCliente);
+        for (int j = 0; j < asiduo[i].CantidadProductosAPedir; j++)
+        {
+            printf("Prod: %s \n", P[j].TipoProducto);
+            printf("Prod: %f \n", P[j].PrecioUnitario);
+        }
+        printf("------------- \n");
+
+        printf("\n");
+    }
+    
 
     scanf("%c");
 
@@ -48,20 +63,17 @@ int main(){
 
 
 
-Cliente * Interfaz(){       //Consulta: Se coloca "*" para indiciar que se devuelve una variable del tipo Cliente puntero?
+Cliente * Interfaz(){
+
     int NdeClientes;
 
     printf("Ingrese la cantidad de clientes: ");
     scanf("%d", &NdeClientes);  
 
-    Cliente * usuario = (Cliente *) malloc(sizeof(Cliente) * NdeClientes);      //Reserva de memoria para la cantidad total de clientes
-    //aqui cuando hago esta reserva de memoria para usuario cuando termina la funcion esto queda reservado, y cuando devuelva el puntero a main y lo libere desde ahi ya queda liberado
+    Cliente * usuario;
+
+    usuario = (Cliente *) malloc(sizeof(Cliente) * NdeClientes);      //Reserva de memoria para la cantidad total de clientes
     CargarClientes(&usuario, NdeClientes);
-
-
-    // printf("Los productos del primer cliente son \n");
-    // printf("Precio unitario: %d\n", usuario[0].Productos[0].PrecioUnitario);
-    // printf("Tipo de producto: %s\n", usuario->Productos->TipoProducto);
 
     return usuario;
 };
@@ -69,50 +81,46 @@ Cliente * Interfaz(){       //Consulta: Se coloca "*" para indiciar que se devue
 
 
 void CargarClientes(Cliente ** p_usuario, int NdeClientes){
-    int ClienteID1, CantidadProductosAPedir;
-    Cliente * p_usuario1 = *p_usuario;  //declaro un puntero auxiliar para enviarlo a la funcion CargarProductos
+    int CantidadProductosAPedir;
 
-    for (int i = 0; i < NdeClientes; i++)
-    {  //------Cargo los datos del Cliente-------
+    for (int i = 0; i < NdeClientes; i++) //------Cargo los datos del Cliente-------
+    {
         printf("Ingrese el ID de cliente Nro. %d: \n", i+1);
-        scanf("%d", &(* p_usuario)->ClienteID);
-        printf("El id del usuario es: %d\n", (* p_usuario)->ClienteID);
+        scanf("%d", &(* p_usuario)[i].ClienteID);
+        printf("El id del usuario es: %d\n", (* p_usuario)[i].ClienteID);
         printf("Ingrese el nombre del cliente #%d: \n", i+1);
-        char *nom_aux = (char *) malloc(sizeof(char) * 100);    //v_aux para guardar el nombre saber la cantidad de caracteres y luego guardar el nombre del cliente
+        char * nom_aux = (char *) malloc(sizeof(char) * 100);
         scanf("%s", nom_aux);
         int tam_nombre = strlen(nom_aux);
-        (* p_usuario)->NombreCliente = (char *) malloc(sizeof(char) * tam_nombre);
-        strcpy((* p_usuario)->NombreCliente, nom_aux);
+        (* p_usuario)[i].NombreCliente = (char *) malloc(sizeof(char) * tam_nombre);
+        strcpy((* p_usuario)[i].NombreCliente, nom_aux);
         free(nom_aux);
 
         printf("Ingrese la cantidad de Productos a Pedir: ");
-        scanf("%d", &(* p_usuario)->CantidadProductosAPedir);
-
-        //-------Cargo los datos del Producto del cliente generados aleatoriamente----
+        scanf("%d", &(* p_usuario)[i].CantidadProductosAPedir);
 
         int CantidadProductos = (* p_usuario)[i].CantidadProductosAPedir;
-        (* p_usuario)[i].Productos = (Producto *) malloc(sizeof(Producto) * CantidadProductos);     //Reservo memoria para los Productos del Cliente
+        (* p_usuario)[i].Productos = (Producto *) malloc(sizeof(Producto) * CantidadProductos);
+        Cliente * p_usuario_aux = (* p_usuario + i);
 
-        CargarProductos(&p_usuario1, CantidadProductos, i);
-        
+        CargarProductos(&p_usuario_aux, CantidadProductos);//-------Cargo los datos del Producto----
+
     }
-    
-    //* p_usuario = * p_usuario - NdeClientes;
 };
 
-void CargarProductos(Cliente ** p_usuario1, int CantidadProductos, int i){
-
+void CargarProductos(Cliente ** p_usuario_aux, int CantidadProductos){
+    //(* p_usuario_aux)->Productos = (Producto *) malloc(sizeof(Producto) * CantidadProductos);     //Reservo memoria para los Productos del Cliente
+    
     for (int j = 0; j < CantidadProductos; j++)
     {
-        int precio_unitario = 10 + rand() % 91;
+        float precio_unitario = 10 + rand() % 91;
         int aleatorio = rand() % 5;
-        (* p_usuario1)[i].Productos[j].ProductoID = j + 1;
-        (* p_usuario1)[i].Productos[j].Cantidad = 1 + rand() % 11;    // TL♥
+        (* p_usuario_aux)->Productos[j].ProductoID = j + 1;
+        (* p_usuario_aux)->Productos[j].Cantidad = 1 + rand() % 11;    // TL♥
         int tamanio = strlen(TiposProductos[aleatorio]);
-        (* p_usuario1)[i].Productos[j].TipoProducto = (char *)malloc(sizeof(tamanio)); //Consulta: Esta bien que hecha la reserva de memoria en este caso?
-        strcpy((* p_usuario1)[i].Productos[j].TipoProducto, TiposProductos[aleatorio]);
-        (* p_usuario1)[i].Productos[j].PrecioUnitario = 10 + rand() % 91;
-        ((*p_usuario1)[i].Productos+j);
+        (* p_usuario_aux)->Productos[j].TipoProducto = (char *)malloc(sizeof(tamanio));
+        strcpy((* p_usuario_aux)->Productos[j].TipoProducto, TiposProductos[aleatorio]);
+        (* p_usuario_aux)->Productos[j].PrecioUnitario = precio_unitario;
     }      
 };
 
